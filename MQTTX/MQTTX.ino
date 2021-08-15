6a81170d632c9cbc81df64bcedf5c92165b13c13
InvincibleNobita <invincible.manish1417@gmail.com>
2021-08-15 20:07:13 +0530

    ENABLED PWM CONTROL

--- MQTTX/MQTTX.ino
+++ MQTTX/MQTTX.ino
@@ -1,11 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "credentials.h"
+#define AWW analogWrite
 #define GLOW digitalWrite
 #define LED1 14
 #define LED2 12
 #define LED3 4
 #define LED4 5
+#define PWM1 9        //DEFINE A PWM GPIO HERE
+#define PWM2 10       //TODO: 
 
 // WiFi
 const char* ssid = WIFI_SSID;
@@ -62,30 +65,44 @@ void callback(char *topic, byte *payload, unsigned int length) {
   }
   switch(s.toInt())
   {
-    case 1:GLOW(LED1,HIGH);
+    case 1:AWW(PWM1,124);
+           AWW(PWM2,124);
+           GLOW(LED1,HIGH);
            GLOW(LED2,LOW);
            GLOW(LED3,HIGH);
            GLOW(LED4,LOW);
     break;
-    case 2:GLOW(LED1,LOW);
+    case 2:AWW(PWM1,124);
+           AWW(PWM2,124);
+           GLOW(LED1,LOW);
            GLOW(LED2,HIGH);
            GLOW(LED3,LOW);
            GLOW(LED4,HIGH);
     break;
-    case 3:GLOW(LED1,HIGH);
+    case 3:AWW(PWM1,80);
+           AWW(PWM2,80);
+          GLOW(LED1,HIGH);
           GLOW(LED2,LOW);
           GLOW(LED3,LOW);
           GLOW(LED4,HIGH);
+          AWW(PWM1,124);
+           AWW(PWM2,124);
     break;
-    case 4: GLOW(LED1,LOW);
+    case 4: AWW(PWM1,80);
+           AWW(PWM2,80);
+            GLOW(LED1,LOW);
             GLOW(LED2,HIGH);
             GLOW(LED3,HIGH);
             GLOW(LED4,LOW);
+            AWW(PWM1,124);
+           AWW(PWM2,124);
     break;
-    default: GLOW(LED1,LOW);
-            GLOW(LED2,LOW);
-            GLOW(LED3,LOW);
-            GLOW(LED4,LOW);
+    default: AWW(PWM1,0);
+           AWW(PWM2,0);
+    // default: GLOW(LED1,LOW);
+    //         GLOW(LED2,LOW);
+    //         GLOW(LED3,LOW);
+    //         GLOW(LED4,LOW);
     break;
   }
   Serial.println();
